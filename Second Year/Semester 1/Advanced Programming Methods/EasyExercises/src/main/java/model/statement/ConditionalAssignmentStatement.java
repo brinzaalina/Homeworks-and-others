1657591d3250f2e36942023f9b1adfa122512fb1
911-Brinza-Alina <72062984+911-Brinza-Alina@users.noreply.github.com>
2022-01-29 16:56:50 +0200

    Conditional Assignment Statement + modification For Statement

--- Second Year/Semester 1/Advanced Programming Methods/EasyExercises/src/main/java/model/statement/ConditionalAssignmentStatement.java
+++ Second Year/Semester 1/Advanced Programming Methods/EasyExercises/src/main/java/model/statement/ConditionalAssignmentStatement.java
@@ -2,35 +2,29 @@ package model.statement;
 
 import exceptions.InterpreterException;
 import model.expression.IExpression;
-import model.expression.RelationalExpression;
 import model.expression.VariableExpression;
 import model.programState.ProgramState;
-import model.type.IntType;
+import model.type.BoolType;
 import model.type.Type;
 import model.utils.MyIDictionary;
 import model.utils.MyIStack;
 
-public class ForStatement implements IStatement{
+public class ConditionalAssignmentStatement implements IStatement{
     private final String variable;
     private final IExpression expression1;
     private final IExpression expression2;
     private final IExpression expression3;
-    private final IStatement statement;
 
-    public ForStatement(String variable, IExpression expression1, IExpression expression2, IExpression expression3, IStatement statement) {
+    public ConditionalAssignmentStatement(String variable, IExpression expression1, IExpression expression2, IExpression expression3) {
         this.variable = variable;
         this.expression1 = expression1;
         this.expression2 = expression2;
         this.expression3 = expression3;
-        this.statement = statement;
     }
-
     @Override
     public ProgramState execute(ProgramState state) throws InterpreterException {
         MyIStack<IStatement> exeStack = state.getExeStack();
-        IStatement converted = new CompoundStatement(new AssignStatement("v", expression1),
-                new WhileStatement(new RelationalExpression("<", new VariableExpression("v"), expression2),
-                        new CompoundStatement(statement, new AssignStatement("v", expression3))));
+        IStatement converted = new IfStatement(expression1, new AssignStatement(variable, expression2), new AssignStatement(variable, expression3));
         exeStack.push(converted);
         state.setExeStack(exeStack);
         return null;
@@ -38,23 +32,23 @@ public class ForStatement implements IStatement{
 
     @Override
     public MyIDictionary<String, Type> typeCheck(MyIDictionary<String, Type> typeEnv) throws InterpreterException {
+        Type variableType = new VariableExpression(variable).typeCheck(typeEnv);
         Type type1 = expression1.typeCheck(typeEnv);
         Type type2 = expression2.typeCheck(typeEnv);
         Type type3 = expression3.typeCheck(typeEnv);
-
-        if (type1.equals(new IntType()) && type2.equals(new IntType()) && type3.equals(new IntType()))
+        if (type1.equals(new BoolType()) && type2.equals(variableType) && type3.equals(variableType))
             return typeEnv;
         else
-            throw new InterpreterException("The for statement is invalid!");
+            throw new InterpreterException("The conditional assignment is invalid!");
     }
 
     @Override
     public IStatement deepCopy() {
-        return new ForStatement(variable, expression1.deepCopy(), expression2.deepCopy(), expression3.deepCopy(), statement.deepCopy());
+        return new ConditionalAssignmentStatement(variable, expression1.deepCopy(), expression2.deepCopy(), expression3.deepCopy());
     }
 
     @Override
     public String toString() {
-        return String.format("for(%s=%s; %s<%s; %s=%s) {%s}", variable, expression1, variable, expression2, variable, expression3, statement);
+        return String.format("%s=(%s)? %s: %s", variable, expression1, expression2, expression3);
     }
 }
