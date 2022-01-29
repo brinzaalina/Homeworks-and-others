970fa6cc76bb5d521d1d781a02c876ea02389133
911-Brinza-Alina <72062984+911-Brinza-Alina@users.noreply.github.com>
2022-01-29 21:01:26 +0200

    synchronization

--- Second Year/Semester 1/Advanced Programming Methods/Toy Language Interpreter A7/model/utils/MyHeap.java
+++ Second Year/Semester 1/Advanced Programming Methods/Toy Language Interpreter A7/model/utils/MyHeap.java
@@ -24,57 +24,75 @@ public class MyHeap implements MyIHeap{
 
     @Override
     public int getFreeValue() {
-        return freeLocationValue;
+        synchronized (this) {
+            return freeLocationValue;
+        }
     }
 
     @Override
     public HashMap<Integer, Value> getContent() {
-        return heap;
+        synchronized (this) {
+            return heap;
+        }
     }
 
     @Override
     public void setContent(HashMap<Integer, Value> newMap) {
-        this.heap = newMap;
+        synchronized (this) {
+            this.heap = newMap;
+        }
     }
 
     @Override
     public int add(Value value) {
-        heap.put(freeLocationValue, value);
-        Integer toReturn = freeLocationValue;
-        freeLocationValue = newValue();
-        return toReturn;
+        synchronized (this) {
+            heap.put(freeLocationValue, value);
+            Integer toReturn = freeLocationValue;
+            freeLocationValue = newValue();
+            return toReturn;
+        }
     }
 
     @Override
     public void update(Integer position, Value value) throws InterpreterException {
-        if (!heap.containsKey(position))
-            throw new InterpreterException(String.format("%d is not present in the heap", position));
-        heap.put(position, value);
+        synchronized (this) {
+            if (!heap.containsKey(position))
+                throw new InterpreterException(String.format("%d is not present in the heap", position));
+            heap.put(position, value);
+        }
     }
 
     @Override
     public Value get(Integer position) throws InterpreterException {
-        if (!heap.containsKey(position))
-            throw new InterpreterException(String.format("%d is not present in the heap", position));
-        return heap.get(position);
+        synchronized (this) {
+            if (!heap.containsKey(position))
+                throw new InterpreterException(String.format("%d is not present in the heap", position));
+            return heap.get(position);
+        }
     }
 
     @Override
     public boolean containsKey(Integer position) {
-        return this.heap.containsKey(position);
+        synchronized (this) {
+            return this.heap.containsKey(position);
+        }
     }
 
     @Override
     public void remove(Integer key) throws InterpreterException {
-        if (!containsKey(key))
-            throw new InterpreterException(key + " is not defined.");
-        freeLocationValue = key;
-        this.heap.remove(key);
+        synchronized (this) {
+            if (!containsKey(key))
+                throw new InterpreterException(key + " is not defined.");
+            freeLocationValue = key;
+            this.heap.remove(key);
+        }
     }
 
     @Override
     public Set<Integer> keySet() {
-        return heap.keySet();
+        synchronized (this) {
+            return heap.keySet();
+        }
     }
     @Override
     public String toString() {
